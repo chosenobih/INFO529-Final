3ea24ebd2ba0c1b0a92776840301f9d7b61f88ea
Ajconnick <70722528+Ajconnick@users.noreply.github.com>
2021-12-12 14:47:37 -0700

    removed irods dependency

--- Data/update_and_build_links.py
+++ Data/update_and_build_links.py
@@ -2,13 +2,9 @@
 Authors: Austin Connick, Ryan Rizzo
 File: This file scraps cyverse for dates and plant names and builds and tests 
 public links
-Note: has not been tested on more than one date but should still find any
-if they exist
-
 requirements/tested on:
         python=2.7.18
         GCC=9.3.0
-        iRODS=4.2.10
         Ubuntu 20.04.3 LTS
 
 '''
@@ -17,113 +13,74 @@ import os
 import subprocess
 import re
 import time
-import requests, urllib, urllib2
-#plant_name, location, index, data, treatment, plot, genotype, lon, lat, min_x, max_x, min_y, nw_lat,nw_lon,se_lat,se_lon,bounding,roi_temp,quartile_1,mean,median,quartile_3,variance,std_dev,labels,double_lettuce
-#https://data.cyverse.org/dav-anon/iplant/projects/phytooracle/season_10_lettuce_yr_2020/level_1/scanner3DTop/recent_changes/2020-03-02/plantcrop/combined_pointclouds/
-
-default_location = "/iplant/home/shared/phytooracle/season_10_lettuce_yr_2020/level_1/scanner3DTop/recent_changes"
+import requests
+import bs4
+from bs4 import BeautifulSoup
+url = "https://data.cyverse.org/dav-anon/iplant/projects/phytooracle/season_10_lettuce_yr_2020/level_1/scanner3DTop/recent_changes/"
 file_to_find = "/combined_multiway_registered.npy"
-
-sample_location = "/iplant/home/shared/phytooracle/season_10_lettuce_yr_2020/level_1/scanner3DTop/recent_changes/2020-03-02/plantcrop/combined_pointclouds"
-
 '''
 getAllDates returns a list of all dates
 returns list
 '''
-#todo add a test for date
-def getAllDates(url):
-    allFoundDates = []
-    os.system("touch plants_date")
-    err = os.system("ils " + url +" >> plants_date")
-    if err != 0:
-        print("error or no subdirectories" + str(err))
-        #os._exit(1)
-        return []
-    #add rex to find data
-    fp = open('plants_date','r')
-    for i in fp:
-        k = i.split("/")
-        if k[0] == "  C- ":
-            f = k[-1]
-            f = f.replace('\n','')
-            allFoundDates.append(f)
-    fp.close()
-    os.system("rm plants_date")
-    return allFoundDates
-
+def getAllDates():
+    domtree = requests.get(url)
+    tree = domtree.text
+    dates = []
+    web = BeautifulSoup(domtree.content, "html.parser")
+    dates =  web.find_all("tr", class_="object collection")
+    list_of_dates = []
+    for i in dates:
+        #print(i.text)
+        i = str(i.text).split("/")[0]
+        print(i)
+        list_of_dates.append(i)
+    return list_of_dates
+    #print(domtree.text)
 '''
 build_names takes a date and returns a list of all plant names under the date
 returns list
 '''
-def build_names(names):
-    #/iplant/home/shared/phytooracle/season_10_lettuce_yr_2020/level_1/scanner3DTop/recent_changes/2020-03-02/plantcrop/combined_pointclouds
-    sub_url_path = default_location +"/"+names+"/plantcrop/combined_pointclouds"
-    os.system("touch plants_name")
-    err = os.system("ils " + sub_url_path +" >> plants_name")
-    if err != 0:
-        print("error or no subdirectories" + str(err))
-        #os._exit(1)
-        return []
-    fp = open("plants_name", 'r')
-    all_names = []
-    for i in fp:
-        k = i.split('/')
-        if k[0] == "  C- ":
-            k = k[-1].replace('\n','')
-            all_names.append(k)
-    fp.close()
-    os.system("rm plants_name")
-    
-    return all_names
-
-
+def build_names(date):
+    #https://data.cyverse.org/dav-anon/iplant/projects/phytooracle/season_10_lettuce_yr_2020/level_1/scanner3DTop/recent_changes/2020-03-02/plantcrop/combined_pointclouds/
+    new_url = url + date + "/plantcrop/combined_pointclouds/"
+    domtree = requests.get(new_url)
+    web = BeautifulSoup(domtree.content, "html.parser")
+    names =  web.find_all("tr", class_="object collection")
+    dates_list = []
+    for i in names:
+        i = str(i.text).split("/")[0]
+        #print(i)
+        dates_list.append(i)
+    return dates_list
+'''
+sets starting path
+'''
+def set_path(url_set):
+    url =""
+    url = url_set
+def set_file_to_find(find):
+    file_to_find = ""
+    file_to_find = find
 '''
 build_public_link takes in a name and date and builds a public link
 returns string
 '''
 #https://data.cyverse.org/dav-anon/iplant/projects/phytooracle/season_10_lettuce_yr_2020/level_1/scanner3DTop/recent_changes/2020-03-02/plantcrop/combined_pointclouds/
-#builds a public link
-#todo fix hard code of path
-def build_public_link(plant_name,plant_date,location):
-    start = "https://data.cyverse.org/dav-anon/"
-    mid = default_location+"/"+plant_date+"/plantcrop/combined_pointclouds/"+plant_name
-    test_link = start + mid + file_to_find
+def build_public_link(plant_name,plant_date):
+    mid = url+"/"+plant_date+"/plantcrop/combined_pointclouds/"+plant_name
+    test_link = mid + file_to_find
     link_code = requests.get(test_link, stream=True).status_code
     if link_code == 404:
         print('invalid link')
         return ''
     else:
         return test_link
-    return test_link
-
-#def prosses_url(url):
-'''
-build_public_link_All takes in the list of names and the date and builds all public links
-returns map of names to links
-returns dict
-'''
-def build_public_link_All(plant_names,plant_date):
-    map_plant_to_public_link = {}
-    for i in plant_names:
-        tmp = build_public_link(i,plant_date)
-        map_plant_to_public_link[i] = tmp
-    return map_plant_to_public_link
-
-#this is just for testing
 '''
 def main():
-   time1 = time.time()
-   #gets a list of all dates in data store
-   plant_dates = getAllDates(default_location)
-   #gets all names under date in index 1
-   date_to_name = build_names(plant_dates[1])
-   #builds and test a public link 
-   public_link = build_public_link(date_to_name[0],plant_dates[1],'')
-   print(public_link)
-   time2 = time.time()
-   total = time2 - time1
-   #timing link testing adds about 1.5s
-   print("time-> "+str(total)+"s")
+   dateL = getAllDates()
+   names = build_names(dateL[0])
+   #print(build_public_link(names[0],dateL[1]))
+   print(names)
 if __name__=="__main__":
     main()
 '''
